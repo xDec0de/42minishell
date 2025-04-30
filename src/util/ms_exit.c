9ff0cc08aba1de66ec1b2a1c30501634aba022ec
xDec0de <xdec0deyt@gmail.com>
2025-04-30 22:31:03 +0200

    Free last input on exit

--- src/util/ms_exit.c
+++ src/util/ms_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ms_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: daniema3 <daniema3@student.42.fr>          +#+  +:+       +#+        */
+/*   By: daniema3 <daniema3@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 20:46:52 by daniema3          #+#    #+#             */
-/*   Updated: 2025/04/30 20:59:45 by daniema3         ###   ########.fr       */
+/*   Updated: 2025/04/30 22:27:35 by daniema3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@ void	ms_free(t_shell	*shell)
 {
 	if (shell == NULL)
 		return ;
+	rl_clear_history();
+	if (shell->last_input != NULL)
+		free(shell->last_input);
 	free(shell);
 }
 

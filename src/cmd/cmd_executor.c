2f5b090aad418ad7edac28450ca37c1a8c161bdb
xDec0de <xdec0deyt@gmail.com>
2025-06-11 18:41:15 +0200

    parse_cmd_input: Free args arrays after execution

--- src/cmd/cmd_executor.c
+++ src/cmd/cmd_executor.c
@@ -6,15 +6,21 @@
 /*   By: daniema3 <daniema3@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/24 22:00:01 by daniema3          #+#    #+#             */
-/*   Updated: 2025/06/11 17:45:57 by daniema3         ###   ########.fr       */
+/*   Updated: 2025/06/11 18:35:52 by daniema3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-t_cmd	*execute_builtins(t_shell *shell, char *cmd, char *context, char **args)
+char	*get_last_output(t_shell *shell)
 {
-	(void) context;
+	if (shell->last_cmd == NULL)
+		return (NULL);
+	return (shell->last_cmd->output);
+}
+
+t_cmd	*execute_builtins(t_shell *shell, char *cmd, char **args)
+{
 	if (ms_strequals(cmd, "exit"))
 		return (bltn_exit(shell, args));
 	if (ms_strequals(cmd, "echo"))
@@ -28,10 +34,16 @@ t_cmd	*execute_builtins(t_shell *shell, char *cmd, char *context, char **args)
 
 t_cmd	*parse_cmd_input(t_shell *shell)
 {
+	t_cmd	*cmd;
 	char	*cmd_name;
 	char	**args;
+	char	**cmd_args;
 
 	args = ms_split(shell->last_input, ' ');
+	cmd_args = ms_arrdup(1, args);
 	cmd_name = args[0];
-	return (execute_builtins(shell, cmd_name, NULL, ms_arrdup(1, args)));
+	cmd = execute_builtins(shell, cmd_name, ms_arrdup(1, args));
+	ms_arrfree(args);
+	ms_arrfree(cmd_args);
+	return (cmd);
 }

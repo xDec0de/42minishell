20d61b93e489b2644099c7a179138e389445b26c
xDec0de <xdec0deyt@gmail.com>
2025-06-12 15:58:04 +0200

    Added simple ASSERT_TRUE & ASSERT_FALSE macros

--- test/test_minishell.h
+++ test/test_minishell.h
@@ -6,7 +6,7 @@
 /*   By: daniema3 <daniema3@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 13:53:14 by daniema3          #+#    #+#             */
-/*   Updated: 2025/06/12 14:08:56 by daniema3         ###   ########.fr       */
+/*   Updated: 2025/06/12 15:52:30 by daniema3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,15 @@
 
 #include "unity.h"
 #include "minishell.h"
+
+# define _STR(x) #x
+# define STR(x) _STR(x)
+
+# define ASSERT_TRUE(expr, num) \
+	TEST_ASSERT_TRUE_MESSAGE((expr), STR(num))
+
+# define ASSERT_FALSE(expr, num) \
+	TEST_ASSERT_FALSE_MESSAGE((expr), STR(num))
 
 void	test_ms_isdigit(void);
 
--- test/util/char/test_ms_isdigit.c
+++ test/util/char/test_ms_isdigit.c
@@ -6,24 +6,24 @@
 /*   By: daniema3 <daniema3@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 13:24:13 by daniema3          #+#    #+#             */
-/*   Updated: 2025/06/12 15:46:15 by daniema3         ###   ########.fr       */
+/*   Updated: 2025/06/12 15:57:35 by daniema3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../test_minishell.h"
 
-void test_ms_isdigit(void)
+void	test_ms_isdigit(void)
 {
-	TEST_ASSERT_FALSE_MESSAGE(ms_isdigit('0' - 1), "1");
-	TEST_ASSERT_TRUE_MESSAGE(ms_isdigit('0'), "2");
-	TEST_ASSERT_TRUE_MESSAGE(ms_isdigit('1'), "3");
-	TEST_ASSERT_TRUE_MESSAGE(ms_isdigit('2'), "4");
-	TEST_ASSERT_TRUE_MESSAGE(ms_isdigit('3'), "5");
-	TEST_ASSERT_TRUE_MESSAGE(ms_isdigit('4'), "6");
-	TEST_ASSERT_TRUE_MESSAGE(ms_isdigit('5'), "7");
-	TEST_ASSERT_TRUE_MESSAGE(ms_isdigit('6'), "8");
-	TEST_ASSERT_TRUE_MESSAGE(ms_isdigit('7'), "9");
-	TEST_ASSERT_TRUE_MESSAGE(ms_isdigit('8'), "10");
-	TEST_ASSERT_TRUE_MESSAGE(ms_isdigit('9'), "11");
-	TEST_ASSERT_FALSE_MESSAGE(ms_isdigit('9' + 1), "12");
+	ASSERT_FALSE(ms_isdigit('0' - 1), 1);
+	ASSERT_TRUE(ms_isdigit('0'), 2);
+	ASSERT_TRUE(ms_isdigit('1'), 3);
+	ASSERT_TRUE(ms_isdigit('2'), 4);
+	ASSERT_TRUE(ms_isdigit('3'), 5);
+	ASSERT_TRUE(ms_isdigit('4'), 6);
+	ASSERT_TRUE(ms_isdigit('5'), 7);
+	ASSERT_TRUE(ms_isdigit('6'), 8);
+	ASSERT_TRUE(ms_isdigit('7'), 9);
+	ASSERT_TRUE(ms_isdigit('8'), 10);
+	ASSERT_TRUE(ms_isdigit('9'), 11);
+	ASSERT_FALSE(ms_isdigit('9' + 1), 12);
 }

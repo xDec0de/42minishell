bedee9f9653d16cc29ac1105fe1fd719bf40d38f
xDec0de <xdec0deyt@gmail.com>
2025-06-14 13:19:12 +0200

    [ci-skip] setup: Added -y and -optional flags

--- setup.sh
+++ setup.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
 # Check if we have permissions first
-if [ $(id -u) -ne 0 ]
-	then echo Root or sudo permissions are required to run this script.
-	exit
+if [ $(id -u) -ne 0 ]; then
+	echo Root or sudo permissions are required to run this script.
+	exit 1
 fi
 
-# Function to check and install de
+# Flags
+
+AUTO_YES=false
+OPTIONALS=false
+
+for arg in "$@"; do
+	if [ "$arg" == "-y" ]; then
+		AUTO_YES=true
+	elif [ "$arg" == "-optional" ]; then
+		OPTIONALS=true
+	else
+		echo "Usage: sudo $0 [-y] [-optional]"
+		exit 2
+	fi
+done
+
+# Function to check and install dependencies
 setup () {
 
 	install_cmd=$1
@@ -14,12 +30,16 @@ setup () {
 	display_name=$3
 
 	if ! eval "$check_command" > /dev/null 2>&1; then
-		echo "$display_name not installed. Install? [y/n]"
-		read -r answer
-		if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
-			$install_cmd
+		if [ "$AUTO_YES" = false ]; then
+			echo "$display_name not installed. Install? [y/n]"
+			read -r answer
+			if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+				$install_cmd
+			else
+				echo "Skipping install of $display_name."
+			fi
 		else
-			echo "Skipping install of $display_name."
+			$install_cmd
 		fi
 	else
 		echo "$display_name is installed."
@@ -32,19 +52,21 @@ setup "apt install -y make" "make -v" "Makefile"
 # Setup readline library
 setup "apt-get install -y libreadline-dev" "dpkg -s libreadline-dev" "libreadline-dev"
 
-# Setup Python (For norminette)
-setup "apt install -y python3" "python3 --version" "[Optional] Python 3 (For norminette)"
+if [ "$OPTIONALS" = true ]; then
+	# Setup Python (For norminette)
+	setup "apt install -y python3" "python3 --version" "[Optional] Python 3 (For norminette)"
 
-# Setup Pip (For norminette)
-setup "apt install -y python3-pip" "pip3 --version" "[Optional] Pip (For norminette)"
+	# Setup Pip (For norminette)
+	setup "apt install -y python3-pip" "pip3 --version" "[Optional] Pip (For norminette)"
 
-# Setup Norminette
-setup "pip3 install norminette --break-system-packages" "command norminette -v" "[Optional] Norminette (GLOBAL INSTALL)"
+	# Setup Norminette
+	setup "pip3 install norminette --break-system-packages" "command norminette -v" "[Optional] Norminette (GLOBAL INSTALL)"
 
-# Setup Unity
-setup "git submodule update --init --recursive" "ls -A test/unity/src" "[Optional] Unity"
+	# Setup Unity
+	setup "git submodule update --init --recursive" "ls -A test/unity/src" "[Optional] Unity"
 
-# Setup lcov
-setup "apt install -y lcov" "lcov -v" "[Optional] lcov"
+	# Setup lcov
+	setup "apt install -y lcov" "lcov -v" "[Optional] lcov"
+fi
 
 make

4aebd9c5a5f23580390e16bd2d91b9237e3d49f8
xDec0de <xdec0deyt@gmail.com>
2025-06-14 17:59:10 +0200

    Fix segfault on ms_strequals

--- src/util/str/ms_strequals.c
+++ src/util/str/ms_strequals.c
@@ -6,7 +6,7 @@
 /*   By: daniema3 <daniema3@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/24 21:46:58 by daniema3          #+#    #+#             */
-/*   Updated: 2025/06/12 14:11:55 by daniema3         ###   ########.fr       */
+/*   Updated: 2025/06/14 17:58:35 by daniema3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ bool	ms_strequals(char *str, char *other)
 	size_t	len;
 	size_t	i;
 
+	if (str == NULL || other == NULL)
+		return (str == other);
 	i = 0;
 	len = ms_strlen(str);
 	if (len != ms_strlen(other))

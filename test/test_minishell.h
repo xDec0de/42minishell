99f7cc7e741736b555a7e6c10c00c6d69175622a
xDec0de <xdec0deyt@gmail.com>
2025-06-14 21:04:09 +0200

    Remove unity git module

--- test/test_minishell.h
+++ test/test_minishell.h
@@ -6,14 +6,13 @@
 /*   By: daniema3 <daniema3@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 13:53:14 by daniema3          #+#    #+#             */
-/*   Updated: 2025/06/14 20:51:24 by daniema3         ###   ########.fr       */
+/*   Updated: 2025/06/14 21:03:53 by daniema3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TEST_MINISHELL_H
 # define TEST_MINISHELL_H
 
-#include "unity.h"
 #include "minishell.h"
 
 /*

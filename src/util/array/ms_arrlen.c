fe81a98f63ffd7cbe07747cd5da8cda7930efc60
xDec0de <xdec0deyt@gmail.com>
2025-06-15 16:51:51 +0200

    ms_arrlen: Return 0 if array is NULL

--- src/util/array/ms_arrlen.c
+++ src/util/array/ms_arrlen.c
@@ -6,7 +6,7 @@
 /*   By: daniema3 <daniema3@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 12:19:03 by daniema3          #+#    #+#             */
-/*   Updated: 2025/06/12 14:11:20 by daniema3         ###   ########.fr       */
+/*   Updated: 2025/06/15 16:50:53 by daniema3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ size_t	ms_arrlen(void **arr)
 {
 	size_t	len;
 
+	if (arr == NULL)
+		return (0);
 	len = 0;
 	while (arr[len] != NULL)
 		len++;

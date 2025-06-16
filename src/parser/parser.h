689936b4a36979e31c045521a9a83b879071703f
xDec0de <xdec0deyt@gmail.com>
2025-06-16 05:22:10 +0200

    parser.h: Added tokenizer data types

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -6,7 +6,7 @@
 /*   By: daniema3 <daniema3@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 05:07:26 by daniema3          #+#    #+#             */
-/*   Updated: 2025/06/16 05:20:33 by daniema3         ###   ########.fr       */
+/*   Updated: 2025/06/16 05:21:16 by daniema3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,27 @@ typedef struct s_command
 	bool				is_heredoc;
 	struct s_command	*next;
 }	t_command;
+
+
+/*
+ - Tokenizer
+ */
+
+typedef enum e_token_type
+{
+	T_WORD,
+	T_PIPE,
+	T_REDIR_IN,
+	T_REDIR_OUT,
+	T_REDIR_APPEND,
+	T_HEREDOC
+}	t_token_type;
+
+typedef struct s_token
+{
+	char			*value;
+	t_token_type	type;
+	struct s_token	*next;
+}	t_token;
 
 #endif

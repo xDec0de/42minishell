71c9101df81fb5ba76feefe026476ad19ae26065
xDec0de <xdec0deyt@gmail.com>
2025-06-21 01:25:05 +0200

    ms_strreplace test: Added mid-string replacement test

--- test/util/str/test_ms_strreplace_vg.c
+++ test/util/str/test_ms_strreplace_vg.c
@@ -6,7 +6,7 @@
 /*   By: daniema3 <daniema3@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 21:29:19 by daniema3          #+#    #+#             */
-/*   Updated: 2025/06/20 23:03:35 by daniema3         ###   ########.fr       */
+/*   Updated: 2025/06/21 01:23:46 by daniema3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int	main(void)
 {
 	FREE_ASSERT_STR_EQUALS(ms_strreplace("Hi, world!", 0, 2, "Hello"), "Hello, world!", 1);
+	FREE_ASSERT_STR_EQUALS(ms_strreplace("Hi, world!", 2, 7, " everyone"), "Hi everyone!", 1);
 	FREE_ASSERT_STR_EQUALS(ms_strreplace("Hi", 0, 2, "Hello"), "Hello", 2);
 	ASSERT_NULL(ms_strreplace("Hi", 0, 3, "Hello"), 3);
 	ASSERT_NULL(ms_strreplace("Hi", 0, 0, NULL), 4);

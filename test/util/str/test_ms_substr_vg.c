cededb4cdf87ddc22e6bcf5d0f53ed44dd203afc
xDec0de <xdec0deyt@gmail.com>
2025-06-22 18:41:22 +0200

    Simplify ms_substr + tests

--- test/util/str/test_ms_substr_vg.c
+++ test/util/str/test_ms_substr_vg.c
@@ -1,30 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ms_substr.c                                        :+:      :+:    :+:   */
+/*   test_ms_substr_vg.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: daniema3 <daniema3@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/17 17:18:10 by daniema3          #+#    #+#             */
-/*   Updated: 2025/06/22 18:38:53 by daniema3         ###   ########.fr       */
+/*   Created: 2025/06/22 18:30:08 by daniema3          #+#    #+#             */
+/*   Updated: 2025/06/22 18:40:14 by daniema3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "ms_assertions.h"
+#include "str_utils.h"
 
-char	*ms_substr(const char *str, unsigned int start, t_ulong len)
+int	main(void)
 {
-	char	*substr;
-	t_ulong	size;
+	ASSERT_NULL(ms_substr(NULL, 0, 0), 1);
+	FREE_ASSERT_STR_EQUALS(ms_substr("Hello, World!", 0, 5), "Hello", 1);
+	FREE_ASSERT_STR_EQUALS(ms_substr("Hello, World!", 7, 42), "World!", 1);
+	FREE_ASSERT_STR_EQUALS(ms_substr("Hello", 10, 5), "", 4);
 
-	if (str == NULL)
-		return (NULL);
-	if (ms_strlen(str) < start)
-		return (ms_strdup(""));
-	size = ms_strlen(&str[start]);
-	if (size < len)
-		len = size;
-	substr = ms_calloc(len + 1, sizeof(char));
-	ms_strlcpy(substr, &str[start], len + 1);
-	return (substr);
 }

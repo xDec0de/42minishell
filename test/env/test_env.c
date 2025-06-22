11a52b8706296bef0b24f2de08883be8fd0a1695
xDec0de <xdec0deyt@gmail.com>
2025-06-22 18:05:15 +0200

    Improve env tests

--- test/env/test_env.c
+++ test/env/test_env.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test_env_free_vg.c                                 :+:      :+:    :+:   */
+/*   test_env.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: daniema3 <daniema3@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/20 23:50:37 by daniema3          #+#    #+#             */
-/*   Updated: 2025/06/22 14:11:16 by daniema3         ###   ########.fr       */
+/*   Created: 2025/06/22 18:00:21 by daniema3          #+#    #+#             */
+/*   Updated: 2025/06/22 18:04:51 by daniema3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,21 @@ int	main(int argc, char **argv, char **env)
 	(void) argv;
 	shell = ms_malloc(sizeof(t_shell));
 	shell->env = NULL;
+	// Init
 	env_init(shell, env);
 	ASSERT_NOT_NULL(shell->env, 1);
+	// Export and get
+	env_export(shell, "TEST_VAR=12345");
+	ASSERT_STR_EQUALS(env_get(shell, "TEST_VAR")->value, "12345", 2);
+	// Redefinie and get
+	env_export(shell, "TEST_VAR=123");
+	ASSERT_STR_EQUALS(env_get(shell, "TEST_VAR")->value, "123", 3);
+	// Unset
+	env_unset(shell, "TEST_VAR");
+	ASSERT_NULL(env_get(shell, "TEST_VAR"), 4);
+	// Free
 	env_free(shell);
-	ASSERT_NULL(shell->env, 2);
+	ASSERT_NULL(shell->env, 5);
 	free(shell);
 	return (0);
 }

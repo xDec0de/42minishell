cae5bf95adc4abca3d88c00251966bbdef267263
WSL HOME <raulrecrec@gmail.com>
2025-06-25 15:21:31 +0200

    Add ms_strjoin function to concatenate two strings

---
+++ src/util/str/ms_strjoin.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ms_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/24 13:49:09 by rexposit          #+#    #+#             */
+/*   Updated: 2025/06/25 15:21:16 by rexposit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	s1_s2_len;
+	size_t	i;
+	size_t	j;
+	char	*ptr;
+
+	s1_s2_len = ft_strlen(s1) + ft_strlen(s2);
+	ptr = malloc(s1_s2_len + 1);
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		ptr[i] = s2[j];
+		i++;
+		j++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}

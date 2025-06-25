1d532b9b8c9a604522427389ed40a03a208f8c6a
WSL HOME <raulrecrec@gmail.com>
2025-06-25 16:04:52 +0200

    Implement token parsing functions: is_sep, skip_spaces, get_token_end, and to_token_array

--- src/parser/input_splitter.c
+++ src/parser/input_splitter.c
@@ -6,7 +6,7 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 14:47:59 by daniema3          #+#    #+#             */
-/*   Updated: 2025/06/25 15:02:07 by rexposit         ###   ########.fr       */
+/*   Updated: 2025/06/25 16:04:51 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,4 +41,59 @@ t_ulong	get_token_count(const char *s)
 		i++;
 	}
 	return (count);
+}
+
+bool	is_sep(char c)
+{
+	return (c == '|' || c == '<' || c == '>');
+}
+
+static t_ulong	skip_spaces(const char *s, t_ulong i)
+{
+	while (s[i] != '\0' && ms_isspace(s[i]))
+		i++;
+	return (i);
+}
+
+static t_ulong	get_token_end(const char *s, t_ulong i)
+{
+	char	quote;
+
+	if (is_sep(s[i]))
+		return (i + 1);
+	quote = 0;
+	while (s[i] != '\0')
+	{
+		if (!quote && ms_isquote(s[i]))
+			quote = s[i];
+		else if (quote && s[i] == quote)
+			quote = 0;
+		else if (!quote && (ms_isspace(s[i]) || is_sep(s[i])))
+			break ;
+		i++;
+	}
+	return (i);
+}
+
+char	**to_token_array(const char *input)
+{
+	t_ulong	i;
+	t_ulong	j;
+	t_ulong	end;
+	char	**tokens;
+
+	tokens = ms_malloc(sizeof(char *) * (get_token_count(input) + 1));
+	i = 0;
+	j = 0;
+	while (input[i] != '\0')
+	{
+		i = skip_spaces(input, i);
+		if (!input[i])
+			break ;
+		end = get_token_end(input, i);
+		tokens[j++] = ms_substr(input, i, end - i);
+		i = end;
+	}
+	tokens[j] = NULL;
+	return (tokens);
 }

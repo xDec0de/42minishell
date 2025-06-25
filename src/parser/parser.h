bee91e11aa431995d8aa17f8d9fb8819ad35730d
xDec0de <xdec0deyt@gmail.com>
2025-06-25 14:04:55 +0200

    Fix norm errors on parser.h

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: daniema3 <daniema3@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 05:07:26 by daniema3          #+#    #+#             */
-/*   Updated: 2025/06/25 13:56:39 by rexposit         ###   ########.fr       */
+/*   Updated: 2025/06/25 14:04:19 by daniema3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,13 +54,13 @@ typedef struct s_token
  - Input validator
  */
 
-bool	check_quotes(const char *line);
+bool			check_quotes(const char *line);
 
 /*
  - Tokenizer
  */
 
-t_ulong	get_token_count(const char *input);
-t_token_type	get_token_type(const char *s)
+t_ulong			get_token_count(const char *input);
+t_token_type	get_token_type(const char *s);
 
 #endif

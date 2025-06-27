f6b8c9cb9347dfdaeeb154f6a4d7226036f5631c
xDec0de <xdec0deyt@gmail.com>
2025-06-27 13:27:44 +0200

    ms_exit: Free last cmd if present

--- src/util/ms_exit.c
+++ src/util/ms_exit.c
@@ -6,7 +6,7 @@
 /*   By: daniema3 <daniema3@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 20:46:52 by daniema3          #+#    #+#             */
-/*   Updated: 2025/06/20 23:48:29 by daniema3         ###   ########.fr       */
+/*   Updated: 2025/06/27 13:24:51 by daniema3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,12 @@ void	ms_free(t_shell	*shell)
 	rl_clear_history();
 	if (shell->last_input != NULL)
 		free(shell->last_input);
+	if (shell->last_cmd != NULL)
+	{
+		if (shell->last_cmd->output != NULL)
+			free(shell->last_cmd->output);
+		free(shell->last_cmd);
+	}
 	env_free(shell);
 	free(shell);
 }

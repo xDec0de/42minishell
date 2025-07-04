c2f0126251f4b16447e7a4e843b73d05265a3c4b
xDec0de <xdec0deyt@gmail.com>
2025-07-04 12:25:25 +0200

    Increment SHLVL on env_init

--- src/env/env_init.c
+++ src/env/env_init.c
@@ -3,14 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   env_init.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: daniema3 <daniema3@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: daniema3 <daniema3@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 16:59:09 by daniema3          #+#    #+#             */
-/*   Updated: 2025/06/27 20:03:13 by daniema3         ###   ########.fr       */
+/*   Updated: 2025/07/04 12:23:36 by daniema3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	increment_shlvl(t_shell *shell)
+{
+	t_env	*shlvl;
+	int		i_new_lvl;
+	char	*new_lvl;
+	char	*new_shlvl;
+
+	shlvl = env_get(shell, "SHLVL");
+	if (shlvl == NULL || !ms_strisnumeric(shlvl->value, false))
+	{
+		env_export(shell, "SHLVL=1");
+		return ;
+	}
+	i_new_lvl = ms_atoi(shlvl->value) + 1;
+	if (i_new_lvl < 0)
+		i_new_lvl = 0;
+	new_lvl = ms_itoa(i_new_lvl);
+	new_shlvl = ms_strjoin("SHLVL", new_lvl, '=');
+	free(new_lvl);
+	env_export(shell, new_shlvl);
+	free(new_shlvl);
+}
 
 void	env_init(t_shell *shell, char **env)
 {
@@ -24,4 +47,5 @@ void	env_init(t_shell *shell, char **env)
 		env_export(shell, env[i]);
 		i++;
 	}
+	increment_shlvl(shell);
 }

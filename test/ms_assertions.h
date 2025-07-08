ead3fe0c86b1521c01411c47661ce21570ee2c31
xDec0de <xdec0deyt@gmail.com>
2025-07-08 13:53:51 +0200

    Improve test fail output: Include literal function call & quotes for strings

--- test/ms_assertions.h
+++ test/ms_assertions.h
@@ -6,7 +6,7 @@
 /*   By: daniema3 <daniema3@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 13:53:14 by daniema3          #+#    #+#             */
-/*   Updated: 2025/07/08 13:23:57 by daniema3         ###   ########.fr       */
+/*   Updated: 2025/07/08 13:46:35 by daniema3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ int __TEST_NUMBER__ = 0;
 #define ASSERT_TRUE(expr) do {\
 	__TEST_NUMBER__++;\
 	if (!(expr)) {\
-		fprintf(stderr, "Got FALSE when expecting TRUE");\
+		fprintf(stderr, "Got FALSE when expecting TRUE from %s", #expr);\
 		exit(__TEST_NUMBER__);\
 	}\
 } while (0)
@@ -39,7 +39,7 @@ int __TEST_NUMBER__ = 0;
 #define ASSERT_FALSE(expr) do {\
 	__TEST_NUMBER__++;\
 	if ((expr)) {\
-		fprintf(stderr, "Got TRUE when expecting FALSE");\
+		fprintf(stderr, "Got TRUE when expecting FALSE from %s", #expr);\
 		exit(__TEST_NUMBER__);\
 	}\
 } while (0)
@@ -51,7 +51,7 @@ int __TEST_NUMBER__ = 0;
 #define ASSERT_NULL(expr) do {\
 	__TEST_NUMBER__++;\
 	if ((expr) != NULL) {\
-		fprintf(stderr, "Got NOT NULL when expecting NULL");\
+		fprintf(stderr, "Got NOT NULL when expecting NULL from %s", #expr);\
 		exit(__TEST_NUMBER__);\
 	}\
 } while (0)
@@ -59,7 +59,7 @@ int __TEST_NUMBER__ = 0;
 #define ASSERT_NOT_NULL(expr) do {\
 	__TEST_NUMBER__++;\
 	if ((expr) == NULL) {\
-		fprintf(stderr, "Got NULL when expecting NOT NULL");\
+		fprintf(stderr, "Got NULL when expecting NOT NULL from %s", #expr);\
 		exit(__TEST_NUMBER__);\
 	}\
 } while (0)
@@ -73,7 +73,7 @@ int __TEST_NUMBER__ = 0;
 	int __aie_actual__ = (actual);\
 	int __aie_expected__ = (expected);\
 	if (__aie_actual__ != __aie_expected__) {\
-		fprintf(stderr, "Got %i when expecting %i", __aie_actual__, __aie_expected__);\
+		fprintf(stderr, "Got %i when expecting %i from %s", __aie_actual__, __aie_expected__, #actual);\
 		exit(__TEST_NUMBER__);\
 	}\
 } while (0)
@@ -83,7 +83,7 @@ int __TEST_NUMBER__ = 0;
 	t_ulong __aue_actual__ = (actual);\
 	t_ulong __aue_expected__ = (expected);\
 	if (__aue_actual__ != __aue_expected__) {\
-		fprintf(stderr, "Got %lld when expecting %lld", __aue_actual__, __aue_expected__);\
+		fprintf(stderr, "Got %lld when expecting %lld from %s", __aue_actual__, __aue_expected__, #actual);\
 		exit(__TEST_NUMBER__);\
 	}\
 } while (0)
@@ -97,14 +97,19 @@ int __TEST_NUMBER__ = 0;
 	char *__ase_actual__ = (actual);\
 	char *__ase_expected__ = (expected);\
 	if (strcmp(__ase_actual__, __ase_expected__) != 0) {\
-		fprintf(stderr, "Got %s when expecting %s", __ase_actual__, __ase_expected__);\
+		fprintf(stderr, "Got \"%s\" when expecting \"%s\" from %s", __ase_actual__, __ase_expected__, #actual);\
 		exit(__TEST_NUMBER__);\
 	}\
 } while (0)
 
 #define FREE_ASSERT_STR_EQUALS(actual, expected) do {\
+	__TEST_NUMBER__++;\
 	char *__fase_actual__ = (actual);\
-	ASSERT_STR_EQUALS(__fase_actual__, (expected));\
+	char *__fase_expected__ = (expected);\
+	if (strcmp(__fase_actual__, __fase_expected__) != 0) {\
+		fprintf(stderr, "Got \"%s\" when expecting \"%s\" from %s", __fase_actual__, __fase_expected__, #actual);\
+		exit(__TEST_NUMBER__);\
+	}\
 	free(__fase_actual__);\
 } while (0)
 

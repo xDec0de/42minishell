95f3e49b18142c4daa9e95c4de1d5b5ebc987032
rexposit <raulrecrec@gmail.com>
2025-07-22 01:27:10 +0200

    fix comment spelling in env test

--- test/env/test_env.c
+++ test/env/test_env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   test_env.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: daniema3 <daniema3@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: rexposit <rexposit@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/22 18:00:21 by daniema3          #+#    #+#             */
-/*   Updated: 2025/07/08 13:24:20 by daniema3         ###   ########.fr       */
+/*   Updated: 2025/07/22 01:27:06 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	main(int argc, char **argv, char **env)
 	env_export(shell, "TEST_VAR=12345");
 	ASSERT_STR_EQUALS(env_get(shell, "TEST_VAR")->key, "TEST_VAR");
 	ASSERT_STR_EQUALS(env_get(shell, "TEST_VAR")->value, "12345");
-	// Redefinie and get
+	// Redefine and get
 	env_export(shell, "TEST_VAR=123");
 	ASSERT_STR_EQUALS(env_get(shell, "TEST_VAR")->key, "TEST_VAR");
 	ASSERT_STR_EQUALS(env_get(shell, "TEST_VAR")->value, "123");

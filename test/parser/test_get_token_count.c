9bf6cdf517132de204d0fce7f72eaab95a7a216c
rexposit <raulrecrec@gmail.com>
2025-07-22 01:28:11 +0200

    add unique case in get\_token\_count

--- test/parser/test_get_token_count.c
+++ test/parser/test_get_token_count.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   test_get_token_count.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: daniema3 <daniema3@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: rexposit <rexposit@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 16:21:22 by daniema3          #+#    #+#             */
-/*   Updated: 2025/07/08 13:25:45 by daniema3         ###   ########.fr       */
+/*   Updated: 2025/07/22 01:28:06 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ int	main(void)
 {
 	ASSERT_ULONG_EQUALS(get_token_count("echo Hello world"), 3L);
 	ASSERT_ULONG_EQUALS(get_token_count("echo \"Hello world\""), 2L);
-	ASSERT_ULONG_EQUALS(get_token_count("echo \"Hello 'world'\""), 2L);
 	ASSERT_ULONG_EQUALS(get_token_count("echo \"Hello 'world'\""), 2L);
 	ASSERT_ULONG_EQUALS(get_token_count("echo \"Hello 'world'\" >> hello.txt"), 4L);
 }

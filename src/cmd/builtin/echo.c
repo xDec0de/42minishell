7031e828bc9bf3230ad8f8eb405933ef2bdc1acf
xDec0de <xdec0deyt@gmail.com>
2025-07-22 18:56:08 +0200

    Echo: Imitate original echo -n flag behaviour

--- src/cmd/builtin/echo.c
+++ src/cmd/builtin/echo.c
@@ -3,33 +3,52 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: daniema3 <daniema3@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: daniema3 <daniema3@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 17:11:48 by daniema3          #+#    #+#             */
-/*   Updated: 2025/06/30 01:13:52 by daniema3         ###   ########.fr       */
+/*   Updated: 2025/07/22 18:54:15 by daniema3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static bool	is_n_flag(char *str)
+{
+	t_ulong	i;
+
+	i = 0;
+	if (str == NULL)
+		return (false);
+	if (str[i] != '-')
+		return (false);
+	i++;
+	if (str[i] != 'n')
+		return (false);
+	while (str[i] == 'n')
+		i++;
+	if (str[i] != '\0')
+		return (false);
+	return (true);
+}
+
 void	bltn_echo(char **args)
 {
-	int		i;
+	t_ulong	i;
 	bool	nl;
 	bool	first_written;
 
 	i = 0;
 	nl = true;
 	first_written = false;
+	while (args[i] != NULL && is_n_flag(args[i]))
+	{
+		nl = false;
+		i++;
+	}
 	while (args[i] != NULL)
 	{
-		if (ms_strequals("-n", args[i]))
-			nl = false;
-		else if (!first_written)
-		{
-			printf("%s", args[i]);
-			first_written = true;
-		}
+		if (!first_written)
+			first_written = printf("%s", args[i]);
 		else
 			printf(" %s", args[i]);
 		i++;

d82468364418a01ee84896829fa5fa5849b6b244
WSL HOME <raulrecrec@gmail.com>
2025-07-24 17:23:32 +0200

    Enhance function documentation

--- src/util/char/char_utils.h
+++ src/util/char/char_utils.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   char_utils.h                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: daniema3 <daniema3@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/22 13:33:41 by daniema3          #+#    #+#             */
-/*   Updated: 2025/07/04 16:47:22 by daniema3         ###   ########.fr       */
+/*   Updated: 2025/07/24 17:23:20 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,44 @@
 /* bool type */
 # include <stdbool.h>
 
+/**
+ * @brief Checks if a character is a digit (0-9).
+ *
+ * @param ch The character to check.
+ *
+ * @return true if the character is a digit, false otherwise.
+ */
 bool	ms_isdigit(char ch);
 
+/**
+ * @brief Checks if a character is a quote (' or ").
+ *
+ * @param c The character to check.
+ *
+ * @return true if the character is a single or double quote, false otherwise.
+ */
 bool	ms_isquote(char c);
 
+/**
+ * @brief Checks if a character is considered whitespace.
+ *
+ * Recognized whitespaces: space, tab, newline, carriage return, etc.
+ *
+ * @param ch The character to check.
+ *
+ * @return true if the character is a whitespace, false otherwise.
+ */
 bool	ms_isspace(char ch);
 
+/**
+ * @brief Checks if a character is a minishell special character.
+ *
+ * Special characters: `|`, `<`, `>`.
+ *
+ * @param ch The character to check.
+ *
+ * @return true if the character is special, false otherwise.
+ */
 bool	ms_isspecial(char ch);
 
 #endif

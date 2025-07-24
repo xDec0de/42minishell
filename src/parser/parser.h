be81688903897a473c8388ce478b1114aa5bfee0
WSL HOME <raulrecrec@gmail.com>
2025-07-24 17:21:20 +0200

    Enhance documentation for token types and functions

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: daniema3 <daniema3@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 05:07:26 by daniema3          #+#    #+#             */
-/*   Updated: 2025/07/18 22:22:35 by daniema3         ###   ########.fr       */
+/*   Updated: 2025/07/24 17:21:00 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,9 @@
 
 # include "ms_types.h"
 
-/*
- - Tokenizer
+/**
+ * @brief Represents the type of a token (command separator or redirection).
  */
-
 typedef enum e_token_type
 {
 	T_END,
@@ -29,6 +28,16 @@ typedef enum e_token_type
 	T_HEREDOC
 }	t_token_type;
 
+/**
+ * @brief Represents a parsed token (command or operator).
+ *
+ * - cmd: The command name (NULL for pure operators).
+ * - args: Array of arguments (NULL-terminated).
+ * - infile: File for input redirection, NULL if none.
+ * - outfile: File for output redirection, NULL if none.
+ * - type: Type of the token (T_PIPE, T_REDIR_OUT, etc.).
+ * - next: Pointer to the next token in the list.
+ */
 typedef struct s_token
 {
 	char			*cmd;
@@ -39,36 +48,75 @@ typedef struct s_token
 	struct s_token	*next;
 }	t_token;
 
-/*
- - Input splitter
- */
 
+/**
+ * @brief Splits a raw input line into an array of string tokens.
+ *
+ * @param input The raw input line (e.g., "ls -l | grep .c").
+ *
+ * @return A NULL-terminated array of token strings.
+ */
 char			**to_token_array(const char *input);
 
-/*
- - Input validator
+/**
+ * @brief Checks for invalid consecutive special characters (pipes or redirects).
+ *
+ * @param input The raw input line to validate.
+ *
+ * @return true if the input is valid, false otherwise.
  */
-
 bool			check_special_char_count(const char *input);
 
 # define SCC_ERR "minishell: Syntax error: Unexpected special character.\n"
 
+/**
+ * @brief Verifies if all quotes are properly closed.
+ *
+ * @param line The input line to validate.
+ *
+ * @return true if quotes are balanced, false otherwise.
+ */
 bool			check_quotes(const char *line);
 
+/**
+ * @brief Validates the entire input line (quotes, syntax, etc.).
+ *
+ * @param line The input line to validate.
+ *
+ * @return true if the input is valid, false otherwise.
+ */
 bool			validate_input(const char *line);
 
-/*
- - Token cleaner
+/**
+ * @brief Frees all allocated memory inside a token list (but not the list itself).
+ *
+ * @param tokens The token list to clean.
  */
-
 void			clean_tokens(t_token *tokens);
 
-/*
- - Tokenizer
+/**
+ * @brief Counts how many tokens are present in the input string.
+ *
+ * @param input The raw input line.
+ *
+ * @return The number of tokens.
  */
-
 t_ulong			get_token_count(const char *input);
+
+/**
+ * @brief Converts an array of string tokens into a linked list of t_token.
+ *
+ * @param tokens The NULL-terminated array of token strings.
+ *
+ * @return A linked list of parsed tokens.
+ */
 t_token			*tokenize(char **tokens);
+
+/**
+ * @brief Frees a linked list of tokens completely.
+ *
+ * @param head The first token in the list.
+ */
 void			free_token_list(t_token *head);
 
 #endif
